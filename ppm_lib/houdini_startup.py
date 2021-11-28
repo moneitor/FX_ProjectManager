1b760757c298e583aa701ede4faada1e56080516
Hernan <3d.hernan@gmail.com>
2021-11-28 19:36:44 +0000

    trying to add ppm to path

--- ppm_lib/houdini_startup.py
+++ ppm_lib/houdini_startup.py
@@ -1,5 +1,6 @@
 import os
 import subprocess as sb
+import sys
 
 
 HOU_NAME = "hfs19.0.383"
@@ -12,20 +13,22 @@ def hou_run(fps=24, resx=1920, resy=1280):
     ppm_lib = os.path.dirname(abs_path)
 
     hou_pipe_path = os.path.join(pwd, "houdini")
-    hou_bash_path = os.path.join(ppm_lib, "bash_scripts/hmaster")
+    hou_bash_path = os.path.join(ppm_lib, "bash_scripts/hmaster")    
+    
 
     hfs = r"/opt/{}".format(HOU_NAME)
     user_expand = "~/{}".format(".".join([  HOU_NAME.split(".")[0],  HOU_NAME.split(".")[1]]  ) )
 
     _env["HH"] = os.pathsep.join([os.path.join(hfs, "houdini"), os.path.expanduser(user_expand)])
-    _env["HOUDINI_PATH"] = ":".join([hou_pipe_path, _env["HH"], _env.get("HOUDINI_PATH", "")])
+    _env["HOUDINI_PATH"] = ":".join([hou_pipe_path, _env["HH"],_env.get("HOUDINI_PATH", "")])
     
     # Set custom environment variables
     _env["FPS"] =  str(fps)
     _env["RESX"], _env["RESY"] = str(resx), str(resy)
-
+    
     # RUN HOUDINI
     result = sb.run(hou_bash_path, shell=True)
+    
         
 
 
