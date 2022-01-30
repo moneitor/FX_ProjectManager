bd95256af37f4ef11e46d3ddf595da8e0261be21
Hernan <3d.hernan@gmail.com>
2022-01-30 19:47:25 +0000

    bash and batch startups for linux and windows

--- ppm_lib/houdini_startup.py
+++ ppm_lib/houdini_startup.py
@@ -3,18 +3,25 @@ import subprocess as sb
 import sys
 from ppm_logger import logger as lg
 from initializers import set_env as set
+from sys import platform
 
 
 HOU_NAME = "hfs19.0.383"
 
 def hou_run(fps=24, resx=1920, resy=1280, job="", first_frame=1001, last_frame=1100, shot_path=""):
     # set all environment variables needed for houdini    
-    set.set_env(fps, resx, resy, job, first_frame, last_frame, shot_path)      
+    set.set_env(fps, resx, resy, job, first_frame, last_frame, shot_path)     
    
-    hou_bash_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bash_scripts/hmaster")      
-
+       
     # RUN HOUDINI
-    result = sb.run(hou_bash_path, shell=True)    
+    if platform == "linux" or platform == "linux2":
+        hou_bash_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bash_scripts/hmaster") 
+        lg.Logger.info("Running LINUX")
+        result = sb.run(hou_bash_path, shell=True)    
+    if platform == "win32":
+        hou_bash_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bash_scripts/hmaster_windows") 
+        lg.Logger.info("Running WINDOWS")
+        result = sb.run(hou_bash_path, shell=True)   
     
         
 if __name__ == "__main__":
