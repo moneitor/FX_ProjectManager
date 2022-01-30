be6794c1556977b95151c7200bcc9b30e1affafc
Hernan <3d.hernan@gmail.com>
2022-01-30 20:22:10 +0000

    more windows fixes

--- ppm_lib/initializers/set_env.py
+++ ppm_lib/initializers/set_env.py
@@ -1,5 +1,5 @@
 import os
-import sys
+from sys import platform
 from ppm_logger import logger as lg
 from dotenv import load_dotenv
 
@@ -34,8 +34,15 @@ def set_env(fps, resx, resy, job, first_frame, last_frame, shot_path):
     ppm_lib = os.path.join(pwd, "ppm_lib")
     
     hfs = HOU_INSTALLATION
-    #user_expand_hou = "~/{}".format(HOU_VERSION)
-    user_expand_hou = os.path.join("~", HOU_VERSION)
+   
+    if platform == "linux" or platform == "linux2":
+        user_expand_hou = os.path.join("~", HOU_VERSION)    
+        
+    
+    if platform == "win32":
+        user_expand_hou = os.path.join("~", "Documents", HOU_VERSION)
+          
+
     
     hh = os.pathsep.join([os.path.join(hfs, "houdini"), os.path.expanduser(user_expand_hou)])
     _env["HH"] = hh
