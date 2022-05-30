7614779447b13890ca38f142b0eb70157b2cd16e
Hernan Llano <33673448+moneitor@users.noreply.github.com>
2022-05-30 13:09:35 +0100

    Create ftl_init.py

---
+++ ppm_lib/snippets/pythonSnippets/ftl_init.py
@@ -0,0 +1,14 @@
+node hou.pwd()
+geo = node.geometry()
+
+geomodule = inlinecpp.createLibrary("FTL_init", acquire_hom_lock=True, catch_crashes=True, include_dirs=[], link_libs=[], includes="""
+#include <GU/GU_Detail.h>
+#include <OP/OP_Director.h>
+
+
+class Utils{
+  public:
+    static void parallelTransport(UT_Vector3 currP, UT_Vector3 parentP, UT_Vector3 parentN, UT_Vector3 parentT, UT_Vector3 &o_newN, UT_Vector3 &o_newT){
+    UT_Vector3 currT;
+    }
+}
