083647b79a4ca96a95fb0e787998daaed02092f5
moneitor <3d.hernan@gmail.com>
2022-06-07 22:18:40 +0100

    adding batch example for windows, and initializing the houdini scenes though 123.py

---
+++ houdini/scripts/123.py
@@ -0,0 +1,16 @@
+import hou
+import os
+import sys
+
+# Initialization of the framerange to the proper range
+hou.playbar.setFrameRange(int(os.getenv("DFSTART")), int(os.getenv("DFEND")))
+hou.playbar.setPlaybackRange(int(os.getenv("DFSTART")), int(os.getenv("DFEND")))
+hou.playbar.jumpToNextKeyframe()
+
+hou.playbar.setRealTime(True)
+
+# Set the desktop to ppm
+desktops_dict = dict((d.name(), d) for d in hou.ui.desktops())
+desktops_dict['ppm'].setAsCurrent()
+
+sys.path.append("../../../../FX_ProjectManager")
