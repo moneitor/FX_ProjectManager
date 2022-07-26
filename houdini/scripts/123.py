1ceebf1cbf975cb22210cb677361cd5127b61596
moneitor <3d.hernan@gmail.com>
2022-07-26 12:32:15 +0100

    updated PYTHONPATH

--- houdini/scripts/123.py
+++ houdini/scripts/123.py
@@ -13,4 +13,12 @@ hou.playbar.setRealTime(True)
 desktops_dict = dict((d.name(), d) for d in hou.ui.desktops())
 desktops_dict['ppm'].setAsCurrent()
 
-#sys.path.append("../../../../FX_ProjectManager")
+hou.setUpdateMode(hou.updateMode.Manual)
+
+global_hou = os.getenv("GLOBAL_COMMON_HOU")
+ppm_dir = os.path.join(os.path.dirname(global_hou), "ppm_lib").replace("\\", "/")
+ppm_dir = os.path.dirname(global_hou)
+
+print(ppm_dir)
+
+sys.path.append(ppm_dir)
