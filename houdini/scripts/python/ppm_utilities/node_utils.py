2645c3dec84e5c5def98f1a2edcd2292ba993ba4
moneitor <3d.hernan@gmail.com>
2023-01-29 18:23:30 +0000

    updated objerge shelf

--- houdini/scripts/python/ppm_utilities/node_utils.py
+++ houdini/scripts/python/ppm_utilities/node_utils.py
@@ -18,17 +18,22 @@ def create_object_merge(ctrl_click):
             pos = node.position()   
             new_pos = pos + hou.Vector2((0, -1.5))
             parent = node.parent()
-            null = parent.createNode("null", "OUT_{}".format(node.name()))
-            null.setPosition(pos)
-            null.move((0, -1.5))
-            null.setColor(hou.Color(0.451, 0.369, 0.769))
+
+            if node.type().name() == "null":
+                null = node
+            else:
+                null = parent.createNode("null", "OUT_{}".format(node.name()))
+                null.setPosition(pos)
+                null.move((0, -1.5))
+                null.setColor(hou.Color(0.451, 0.369, 0.769))
             
             omerge = parent.createNode("object_merge", "om_{}".format(node.name()))
             omerge.setPosition(pos)
             omerge.move((0, -2.5))
             omerge.setColor(hou.Color(0.451, 0.369, 0.769))
             
-            null.setInput(0, node)
+            if node.type().name() != "null":
+                null.setInput(0, node)
             
             relpath = omerge.relativePathTo(null)
             
